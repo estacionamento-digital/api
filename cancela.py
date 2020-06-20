08e8e3b98606633a3aeed18f912e74ccec5bf496
gustavorattmann <gustavo_rattmann@hotmail.com.br>
2020-06-20 16:00:48 -0300

    Criado conexão do Python com o Cloud Firestore, em seguida é realizado uma busca da coleção de cancela e verificado se está aberta ou fechada

--- cancela.py
+++ cancela.py
@@ -1,5 +1,10 @@
+import serial
+import time
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
+time.sleep(2)
 
 cred = credentials.Certificate("pi-park-2020-firebase-adminsdk-y8zea-8181185594.json")
 default_app = firebase_admin.initialize_app(cred)
@@ -9,6 +14,6 @@ cancela_ref = db.collection(u'cancela').document(u'i8lmwUc59Ptf4Qpp4LgH')
 cancela = cancela_ref.get(field_paths={'cancela'}).to_dict().get('cancela')
 
 if(cancela == 'aberta'):
-    print("Cancela aberta")
+    arduino.write(b'1')
 else:
-    print("Cancela fechada")
+    arduino.write(b'0')
